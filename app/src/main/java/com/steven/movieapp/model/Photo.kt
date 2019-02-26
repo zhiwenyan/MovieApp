a364094c23a3a99c0d2ad713ad24e4e7da36b1de
“zhiwenyan” <zhiwenyan@icloud.com>
2019-02-26 11:45:47 +0800

    影人

---
+++ app/src/main/java/com/steven/movieapp/model/Photo.kt
@@ -0,0 +1,17 @@
+package com.steven.movieapp.model
+
+/**
+ * Description:
+ * Data：2019/2/26
+ * Author:Steven
+ */
+data class Photo(
+    val thumb: String,
+    val image: String,
+    val cover: String,
+    val alt: String,
+    val id: String,
+    val icon: String
+
+
+)
